85a7c75978b3be395786fc0af5fab2c28464894a
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2021-05-21 14:24:41 -0500

    Agregando templates

--- posts/views.py
+++ posts/views.py
@@ -1,40 +1,42 @@
 """ Post views  """
 
 #Django import
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 
 #Utilites
 from datetime import datetime
 
 posts = [
     {
-        'name':'Mont Blac',
-        'user':'Juan Viro',
+        'title':'Mont Blac',
+        'user':{
+            'name':'Juan Viro',
+            'picture':'https://i.picsum.photos/id/490/60/60.jpg?grayscale&hmac=bRsTn-eEXK4Uo_EqO0FKHpua2_H_sEZHdVFdiccGWgM'
+        },
         'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture':'https://i.picsum.photos/id/503/200/300.jpg?hmac=NvjgwV94HmYqnTok1qtlPsDxdf197x8fsWy5yheKlGg'
+        'photo':'https://i.picsum.photos/id/603/800/800.jpg?hmac=VANxlVkT7NcDHxNp7-oV75u5CXh50jJQ3nOO6FsfTNw'
     },
     {
-        'name':'Jet',
-        'user':'Katherine SAnchez',
+        'title':'Jet',
+        'user':{
+            'name':'Katherine Sanchez',
+            'picture':'https://i.picsum.photos/id/974/60/60.jpg?grayscale&hmac=OvVulJOwUv-dTMPgvNHrPoQ7WSX8pJzK0hkKxLwnY3Y'
+        },
         'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture':'https://i.picsum.photos/id/830/200/300.jpg?hmac=YHS3854_x-GHeQToxsiUmEvBJpDbZOAyixX9nxz61Sg'
+        'photo':'https://i.picsum.photos/id/1022/800/800.jpg?hmac=C_htzo7js3N5v19Y3ie4R9zuhsxLbXTu6VODE5CXXJE'
     },
     {
-        'name':'Mont Blac',
-        'user':'Juan Viro',
-        'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture':'https://i.picsum.photos/id/825/200/300.jpg?hmac=02AaqBOvx8gwrGt7a3HWzJHnZXrMzYoXbAYwjJWH40E'
+        'title':'Mont Blac',
+        'user':{
+            'name':'Alejandro Ospina',
+            'picture':'https://i.picsum.photos/id/220/60/60.jpg?grayscale&hmac=zePoZddC0TDbLpXObeIT5WZSl44e4sSHIGU7AlwaipM'
+        },
+        'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs')  ,
+        'photo': 'https://i.picsum.photos/id/821/800/800.jpg?hmac=dprIWVMGR_ZtJAoT9PdNXWxA0SepdlG5EwPw68OnVLc'      
     }        
 ]
 
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append(""" 
-            <p> <strong> {name} </strong></p>
-            <p> <small> {user} - <i>{timestamp}</i> </small></p>
-            <figure> <img src="{picture}"/> </figure>
-         """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request,'feed.html',{'posts':posts})
 
 
